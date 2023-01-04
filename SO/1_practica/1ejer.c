5a471b28ecc6bd82e44e663c08c3e8d1a3684106
Manuel Nevado Fabian <manuneva@ucm.es>
2023-01-04 12:31:57 +0100

    dejarlo bonito

--- SO/1_practica/1ejer.c
+++ SO/1_practica/1ejer.c
@@ -7,7 +7,14 @@ int main(){
 
     setuid(0);
     if(errno){
-        printf("%s\n",strerror(errno));
+        printf("The instruction [setuid(0)] has raised an errno\n");
+        printf("The errno code is: %d\n", errno);
+        printf("The strerror associated is: %s\n",strerror(errno));
+        printf("The perror way to raise it would be:\n");
+        printf("~~~~~~~~~~~~~~~~~~\n");
+        perror("FAIL!");
+        printf("~~~~~~~~~~~~~~~~~~\n");
+        return 1;
     }
-    return 1;
+    return 0;
 }
